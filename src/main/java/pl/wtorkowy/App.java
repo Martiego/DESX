c8e01419137dddcff7530d0f6730d0a6f61e230c
kacper1410 <kacper1410@users.noreply.github.com>
2019-10-23 15:50:57 +0200

    New gradle version
    gradle-6.0-20191022230249+0000 snapshot

--- src/main/java/pl/wtorkowy/App.java
+++ src/main/java/pl/wtorkowy/App.java
@@ -9,7 +9,7 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         primaryStage.show();
     }
 }
