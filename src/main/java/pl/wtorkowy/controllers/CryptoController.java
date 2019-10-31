c60bf33b6c5a7d04009f03ac0d9ad03191e558e7
Martiego <marti3go@gmail.com>
2019-10-31 21:46:44 +0100

    Added new JavaFX pane

--- src/main/java/pl/wtorkowy/controllers/CryptoController.java
+++ src/main/java/pl/wtorkowy/controllers/CryptoController.java
@@ -1,30 +1,55 @@
 package pl.wtorkowy.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import pl.wtorkowy.cast.ToTab;
 import pl.wtorkowy.crypto.Des;
+import pl.wtorkowy.crypto.Desx;
 
 public class CryptoController {
     @FXML
     private Button encryptBtn;
     @FXML
-    private TextField textTxt;
+    private Button decryptBtn;
+    @FXML
+    private TextArea textTxt;
     @FXML
     private TextField keyTxt;
     @FXML
-    private Label criptTextLbl;
+    private TextField internalKeyTxt;
+    @FXML
+    private TextField externalKeyTxt;
+    @FXML
+    private Label cipherTextLbl;
+    private byte[] cipherText;
+    private byte[] decipherText;
 
     @FXML
-    public void onClick() {
-        Des des = new Des();
+    public void onClickEncrypt() {
+        Desx desx = new Desx();
         char[] text = ToTab.toCharTab(textTxt.getText());
         char[] key = ToTab.toCharTab(keyTxt.getText());
-        byte[] cipherText;
+        char[] externalKey = ToTab.toCharTab(externalKeyTxt.getText());
+        char[] internalKey = ToTab.toCharTab(internalKeyTxt.getText());
 
-        cipherText = des.encrypt(text, key);
-        //criptTextLbl.setText(des.getCipherTextString());
+        cipherText = desx.encrypt(text, internalKey, key, externalKey);
+
+        cipherTextLbl.setText(desx.getCipherTextString());
+    }
+
+    @FXML
+    public void onClickDecrypt() {
+        Desx desx = new Desx();
+        char[] key = ToTab.toCharTab(keyTxt.getText());
+        char[] externalKey = ToTab.toCharTab(externalKeyTxt.getText());
+        char[] internalKey = ToTab.toCharTab(internalKeyTxt.getText());
+
+        decipherText = desx.decrypt(cipherText, internalKey, key, externalKey);
+
+        cipherTextLbl.setText(desx.getDecipherTextString());
     }
 }
